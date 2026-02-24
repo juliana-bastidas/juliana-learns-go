659b825dc9f62ba18b8e7e4157142ce289a5936a
Juli <juli.bastidas12@gmail.com>
2026-02-23 22:15:53 -0500

    flow control and math operators

--- sintaxis/main.go
+++ sintaxis/main.go
@@ -50,4 +50,75 @@ func main() {
 
 	fmt.Println(esMayorQue)
 
+	// Math operators
+	//arithmetic operators
+
+	fmt.Println(1 + 2) //suma
+	fmt.Println(3 - 2) //resta
+	fmt.Println(3 * 2) //multiplicacion
+	fmt.Println(4 / 2) // division =2
+	fmt.Println(4 % 2) // modulo = 0
+	//comparison operators
+	fmt.Println(1 == 2) //igualdad
+	fmt.Println(1 != 2) //desigualdad
+	fmt.Println(1 >= 2) //mayor igual que
+	fmt.Println(1 <= 2) //menor igual que
+
+	//logical operators , el sombreado naranja es una advertencia no un error por lo tanto deja compilar
+	fmt.Println(true && true) //AND
+	fmt.Println(true || true) // OR
+	fmt.Println(!true)        //NOT
+
+	/*
+		FLOW CONTROL
+	*/
+
+	//if
+
+	if edad < 18 {
+		fmt.Println("No puedes entrar, eres menor de edad")
+	} else {
+		fmt.Println("Adelante")
+	}
+
+	//for
+	for i := 0; i <= 3; i++ {
+		fmt.Println(i)
+	}
+
+	//while: NO EXISTE EN GO, el for se comporta como un while
+	total := 0
+	for total < 10 {
+		fmt.Println(total)
+		total++
+	}
+
+	//for ...range
+	nombres := []string{"a", "b", "c"}
+	for _, item := range nombres {
+		fmt.Println(item)
+	}
+
+	//swith
+
+	mes := "Agosto"
+	switch mes {
+	case "Enero":
+		fmt.Println("Feliz año nuevo")
+	case "Diciembre":
+		fmt.Println("Fin de año")
+	default:
+		fmt.Println("Cualquier otro mes")
+	}
+	dia := `asd {edad \n asd}`
+	fmt.Println(dia)
+
+	fmt.Printf("Tengo %d años de edad \n", edad)
+	formattedDate := fmt.Sprintf("Tengo %d años de edad", edad)
+	fmt.Println(formattedDate)
+	/*
+		fmt.Printf: envia inmediatamente el string con formato a la consola
+		espera el formato completo incluso los saltos de linea.
+		fmt.Sprintf: asigna el resultado a una variable, para despues utilizar esa variable
+	*/
 }
